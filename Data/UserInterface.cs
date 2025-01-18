5c6fccd178565989241994db52446f81d6ab3af2
Fredrik von Post <fredrik.v.post@gmail.com>
2025-01-18 17:58:50 +0100

    Added Windows aand UserInterface class

---
+++ Data/UserInterface.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace consoleshoppen.Data;
+
+
+internal class UserInterface
+{
+    private ShopDbContext _dbContext;
+    public UserInterface(ShopDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public void ShowCategories()
+    {
+        var categories = _dbContext.ProductCategories.ToList();
+        var results = new List<string>();
+        foreach (var category in categories)
+        {
+            results.Add($"{category.Id}. {category.Name}");
+        }
+        var categoryWindow = new Window("Kategorier", 1, 1, results);
+        categoryWindow.Draw();
+    }
+    public void ShowProducts(int categoryId)
+    {
+        var products = _dbContext.Products.Include(p => p.ProductCategories)
+                                          .ToList()
+                                          .Where(p => p.ProductCategories.Any(pc => pc.Id == categoryId));
+        var results = new List<string>();
+        foreach (var product in products)
+        {
+            results.Add($"{product.Id,-2}. {product.Name,-20} {product.Price,-5}");
+        }
+        var productWindow = new Window("Produkter", 1, 12, results);
+        productWindow.Draw();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,16 +8,17 @@ internal class Program
     private static void Main(string[] args)
     {
         using var dbContext = new ShopDbContext();
+        var ui = new UserInterface(dbContext);
+        ui.ShowCategories();
+        ui.ShowProducts(1);
         //var dependentSeeder = new DependentDataSeeder(dbContext);
         //dependentSeeder.Seed();
-        var productSeeder = new ProductDataSeeder(dbContext);
-        productSeeder.Seed();
-        var displayer = new TableContentDisplayer(dbContext);
-
-        displayer.DisplayProducts();
-        displayer.DisplayCategories();
-        displayer.DisplayColors();
-        displayer.DisplaySizes();
+        //var productSeeder = new ProductDataSeeder(dbContext);
+        //productSeeder.Seed();
+        //displayer.GetProducts();
+        //displayer.GetCategories();
+        //displayer.GetColors();
+        //displayer.GetSizes();
 
     }
     
